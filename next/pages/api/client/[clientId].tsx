9fddd429473fa889520522a90a8ccaa3e1f5e554
Esther Osammor <54954631+esthero3@users.noreply.github.com>
2022-08-04 14:23:53 -0600

    CN 211 - Create a new API route for fetching a specific card's data

---
+++ next/pages/api/client/[clientId].tsx
@@ -0,0 +1,17 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Client } from "../types/Client";
+import { clientsData } from "../data/clientsData";
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Client | null>
+) {
+  const { clientId } = req.query;
+  const client = clientsData.find((client: Client) => client._id === clientId);
+
+  if (!client) {
+    res.status(404).json(null);
+  }
+
+  if (client) res.status(200).json(client);
+}
---
+++ next/pages/api/data/clientsData.ts
@@ -0,0 +1,44 @@
+import { Client } from "../types/Client";
+
+export const clientsData: Client[] = [
+  {
+    documentRef: "62e2c088074ae1c859d86c42",
+    emailAddress: "rheba@gmail.com",
+    firstName: "Rheba",
+    lastName: "Sherwood",
+    meetingDate: "Aug 25th, 2022",
+    phoneNumber: { cell: "872477751" },
+    progress: 55,
+    _id: "62e2c102a45ab78dc565b27c",
+  },
+  {
+    documentRef: "62e2c088074ae1c859d86c42",
+    emailAddress: "temi@gmail.com",
+    firstName: "Temi",
+    lastName: "Adeola",
+    meetingDate: "Aug 28th, 2022",
+    phoneNumber: { cell: "182422751" },
+    progress: 50,
+    _id: "62e2c102a45ab78dc565b21f",
+  },
+  {
+    documentRef: "62e2c088074ae1c859d86c42",
+    emailAddress: "rocky@gmail.com",
+    firstName: "Rocky",
+    lastName: "Chen",
+    meetingDate: "Aug 12th, 2022",
+    phoneNumber: { cell: "8123477751" },
+    progress: 66,
+    _id: "62e2c102a45ab78dc565b211",
+  },
+  {
+    documentRef: "62e2c088074ae1c859d86c42",
+    emailAddress: "dakos@gmail.com",
+    firstName: "Dakos",
+    lastName: "Sherwood",
+    meetingDate: "Aug 30th, 2022",
+    phoneNumber: { cell: "116477751" },
+    progress: 22,
+    _id: "6211c102a45ab78dc565b27c",
+  },
+];
