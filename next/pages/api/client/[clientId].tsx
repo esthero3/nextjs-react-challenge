dd6d1d2be07dffd8961a2983b10b7bede243553f
Esther Osammor <54954631+esthero3@users.noreply.github.com>
2022-08-04 14:34:09 -0600

    CN 211*
    changed the type of when client data does not exist from null to undefined

--- next/pages/api/client/[clientId].tsx
+++ next/pages/api/client/[clientId].tsx
@@ -4,13 +4,13 @@ import { clientsData } from "../data/clientsData";
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Client | null>
+  res: NextApiResponse<Client | undefined>
 ) {
   const { clientId } = req.query;
   const client = clientsData.find((client: Client) => client._id === clientId);
 
   if (!client) {
-    res.status(404).json(null);
+    res.status(404).json(undefined);
   }
 
   if (client) res.status(200).json(client);
