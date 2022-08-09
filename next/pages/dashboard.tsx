6b615d922102ca3e35945fab6b9c6fc45963e20b
Esther Osammor <54954631+esthero3@users.noreply.github.com>
2022-08-09 16:21:15 -0600

    init

--- next/pages/dashboard.tsx
+++ next/pages/dashboard.tsx
@@ -1,10 +1,47 @@
 import React from "react";
+import { server } from "../config";
+import { GetStaticProps, NextPage } from "next";
+import { ClientsContext } from "../_pages/Admin/contexts/ClientsContext";
 import { DashboardPage } from "../_pages/Admin/DashboardPage";
+import { Client } from "./api/types/Client";
+import AdminLayout from "../Layouts/AdminLayout";
 
-export default function Dashboard() {
+interface Props {
+  //clients: Client[];
+  children: React.ReactNode; //allows you to nest other components inside this(AppPage)
+}
+
+// export const Dashboard: NextPage<Props> = ({ clients }) => {
+//   return (
+//     <>
+//       <ClientsContext.Provider //the context for the clients
+//         value={{
+//           clients: clients,
+//         }}
+//       >
+//         <DashboardPage />
+//       </ClientsContext.Provider>
+//     </>
+//   );
+// };
+
+export const Dashboard = (props: Props) => {
   return (
     <>
-      <DashboardPage />
+      <AdminLayout></AdminLayout>
     </>
   );
-}
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const rawData = await fetch(`${server}/api/clients`);
+  const clients = await rawData.json();
+
+  return {
+    props: {
+      clients,
+    },
+  };
+};
+
+export default Dashboard;
